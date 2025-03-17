0c6ef15af7ef13817c19a08a12ec054060d14674
Ricardo Perez <rpe19971210@gmail.com>
2025-03-17 00:26:20 -0300

    Inicio actualizado

--- src/Components/Inicio/Inicio.jsx
+++ src/Components/Inicio/Inicio.jsx
@@ -1,6 +1,6 @@
 export const Inicio = () => {
     return (
-        <div className="d-flex align-items-center justify-content-center mt-3 p-3">
+        <div className="d-flex align-items-center justify-content-center mt-5 p-5">
             <div className="text m-5">
                 <h4>👋Hola, soy</h4>
                 <h1>Ricardo</h1>
