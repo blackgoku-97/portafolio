c7f686f665250ab12c563438841984b8071ea19a
Ricardo Perez <rpe19971210@gmail.com>
2025-03-17 20:17:02 -0300

    responsivo

--- src/Components/Inicio/Inicio.jsx
+++ src/Components/Inicio/Inicio.jsx
@@ -1,16 +1,16 @@
 export const Inicio = () => {
     return (
-        <div className="d-flex align-items-center justify-content-center mt-5 p-5">
-            <div className="text m-5">
-                <h4>👋Hola, soy</h4>
-                <h1>Ricardo</h1>
-                <h5>
-                    <>Desarrollador Web</>
-                </h5>
-            </div>
-            <div className="img">
-                <img src="img/coding.png" alt="img" />
-            </div>
+      <div className="container mt-5 p-5">
+        <div className="row align-items-center justify-content-center">
+          <div className="col-md-6 text-center">
+            <h4 className="text-sm">Hola, soy</h4>
+            <h1>Ricardo</h1>
+            <h5 className="text-sm">Desarrollador Web</h5>
+          </div>
+          <div className="col-md-6">
+            <img src="img/coding.png" alt="img" className="img-fluid" />
+          </div>
         </div>
-    )
-}
+      </div>
+    );
+  };
