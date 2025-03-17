ca4080c8527f8fdddf3fe65aa392700c68da15ea
Ricardo Perez <rpe19971210@gmail.com>
2025-03-17 20:21:58 -0300

    responsive

--- src/Components/Inicio/Inicio.jsx
+++ src/Components/Inicio/Inicio.jsx
@@ -3,7 +3,7 @@ export const Inicio = () => {
       <div className="container mt-5 p-5">
         <div className="row align-items-center justify-content-center">
           <div className="col-md-6 text-center">
-            <h4 className="text-sm">Hola, soy</h4>
+            <h4 className="text-sm">👋Hola, soy</h4>
             <h1>Ricardo</h1>
             <h5 className="text-sm">Desarrollador Web</h5>
           </div>
