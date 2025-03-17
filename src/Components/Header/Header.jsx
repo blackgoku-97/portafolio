d7c3b7346912dbe3fcb32188f1e0ace67d0193a1
Ricardo Perez <rpe19971210@gmail.com>
2025-03-17 16:34:31 -0300

    Vercel

--- src/Components/Header/Header.jsx
+++ src/Components/Header/Header.jsx
@@ -1,12 +1,10 @@
-import './Header.css';
-
 export const Header = () => {
     return (
         <>
             <nav className="navbar navbar-expand-lg fixed-top mt-2 p-2">
                 <div className="container-fluid">
                     <div className="collapse navbar-collapse" id="navbarSupportedContent">
-                        <ul className="navbar-nav me-auto mb-2 mb-lg-0">
+                        <ul className="navbar-nav ms-auto mb-2 mb-lg-0">
                             <li className="nav-item">
                                 <a className="nav-link active text-light bg-primary rounded-pill mx-auto p-2" aria-current="page" href="#">Inicio</a>
                             </li>
