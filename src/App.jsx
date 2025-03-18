24f417998cb6db88c84ae0f079ceead8843a7efa
Ricardo Perez <rpe19971210@gmail.com>
2025-03-17 21:55:25 -0300

    router

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,20 @@
+import { Navigate, Route, Routes } from 'react-router-dom';
 import { Header } from './Components/Header/Header';
 import { Inicio } from './Components/Inicio/Inicio';
+import { Proyectos } from './Components/Projects/Proyectos';
 
 function App() {
 
     return (
         <>
             <Header />
-            <Inicio />
-        </>
+            
+            <Routes>
+                <Route path="/" element={<Inicio />} />
+                <Route path="/proyectos" element={<Proyectos />} />
+                <Route path="*" element={<Navigate to="/" />} />
+            </Routes>
+        </>   
     )
 }
 
--- src/Components/Header/Header.jsx
+++ src/Components/Header/Header.jsx
@@ -1,3 +1,5 @@
+import { Link } from 'react-router-dom';
+
 export const Header = () => {
   return (
     <>
@@ -17,18 +19,18 @@ export const Header = () => {
           <div className="collapse navbar-collapse" id="navbarSupportedContent">
             <ul className="navbar-nav ms-auto mb-2 mb-lg-0">
               <li className="nav-item">
-                <a
+                <Link
                   className="nav-link active text-light bg-primary rounded-pill mx-auto p-2"
                   aria-current="page"
-                  href="#"
+                  to="/"
                 >
                   Inicio
-                </a>
+                </Link>
               </li>
               <li className="nav-item">
-                <a className="nav-link text-light" href="#">
+                <Link className="nav-link text-light" to="/proyectos">
                   Proyectos
-                </a>
+                </Link>
               </li>
               <li className="nav-item">
                 <a className="nav-link text-light" href="#">
