acd6b6c047dad5df6cb68351f4ccbd59db9a8290
Ricardo Perez <rpe19971210@gmail.com>
2025-03-19 17:05:17 -0300

    contacto 1

--- src/components/Contacto.jsx
+++ src/components/Contacto.jsx
@@ -1,5 +1,30 @@
+import { NavLink } from 'react-router-dom';
+
 export const Contacto = () => {
   return (
-    <h1>Contacto</h1>
+    <div className="text-center">
+      <h1 className="mb-4">¡Contáctame!</h1>
+      <p>Puedes contactarme a través de los siguientes medios:</p>
+      <div className="row justify-content-center">
+        <div className="col-md-4 mb-4">
+          <NavLink to="mailto:rpe19971210gmail.com" target="_blank">
+            <img className='w-25' src="https://skillicons.dev/icons?i=gmail" alt="Gmail" />
+          </NavLink>
+          <h5>Correo Electronico</h5>
+        </div>
+        <div className="col-md-4 mb-4">
+          <NavLink to="https://www.linkedin.com/in/ricardo-perez-a31474229/" target="_blank">
+            <img className='w-25' src="https://skillicons.dev/icons?i=linkedin" alt="LinkedIn" />
+          </NavLink>
+          <h5>LinkedIn</h5>
+        </div>
+        <div className="col-md-4 mb-4">
+          <NavLink to="https://github.com/blackgoku-97" target="_blank">
+            <img className='w-25' src="https://skillicons.dev/icons?i=github" alt="GitHub" />
+          </NavLink>
+          <h5>GitHub</h5>
+        </div>
+      </div>
+    </div>
   )
 }
