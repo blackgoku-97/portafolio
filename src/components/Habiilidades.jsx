6bcb9b4068f71837d0be0465472cada1f5bd2e41
Ricardo Perez <rpe19971210@gmail.com>
2025-03-18 21:00:04 -0300

    habilidades 6

--- src/components/Habiilidades.jsx
+++ src/components/Habiilidades.jsx
@@ -6,26 +6,99 @@ export const Habilidades = () => {
       <h1 className="titulo text-center">Habilidades</h1>
       <div className="row justify-content-center mt-5">
         <div className="col-sm-2 mb-4">
-          <div className="card" style={{ border: 'none !important' }}>
+          <div className="card">
             <img
               src="https://skillicons.dev/icons?i=html"
               alt="html-5"
             />
+            <p className='text-light text-center'>HTML 5</p>
           </div>
         </div>
-        <div className="col-sm-2 mb-4 mx-5">
+        <div className="col-sm-2 mb-4 mx-3">
           <div className="card">
             <img src="https://skillicons.dev/icons?i=css" alt="css3" />
-            <p>CSS 3</p>
+            <p className='text-light text-center'>CSS 3</p>
           </div>
         </div>
-        <div className="col-sm-2 mb-4 mx-5">
-          <div className="card" style={{ border: 'none !important' }}>
+        <div className="col-sm-2 mb-4 mx-3">
+          <div className="card">
             <img
               src="https://skillicons.dev/icons?i=javascript"
               alt="javascript"
             />
-            <p>Javascript</p>
+            <p className='text-light text-center'>JavaScript</p>
+          </div>
+        </div>
+        <div className="col-sm-2 mb-4 mx-3">
+          <div className="card">
+            <img
+              src="https://skillicons.dev/icons?i=react"
+              alt="react"
+            />
+            <p className='text-light text-center'>React</p>
+          </div>
+        </div>
+        <div className="col-sm-2 mb-4 mx-3">
+          <div className="card">
+            <img
+              src="https://skillicons.dev/icons?i=java"
+              alt="java"
+            />
+            <p className='text-light text-center'>Java</p>
+          </div>
+        </div>
+        <div className="col-sm-2 mb-4 mx-3">
+          <div className="card">
+            <img
+              src="https://skillicons.dev/icons?i=spring"
+              alt="spring"
+            />
+            <p className='text-light text-center'>Spring</p>
+          </div>
+        </div>
+        <div className="col-sm-2 mb-4 mx-3">
+          <div className="card">
+            <img
+              src="https://skillicons.dev/icons?i=git"
+              alt="git"
+            />
+            <p className='text-light text-center'>Git</p>
+          </div>
+        </div>
+        <div className="col-sm-2 mb-4 mx-3">
+          <div className="card">
+            <img
+              src="https://skillicons.dev/icons?i=github"
+              alt="github"
+            />
+            <p className='text-light text-center'>GitHub</p>
+          </div>
+        </div>
+        <div className="col-sm-2 mb-4 mx-3">
+          <div className="card">
+            <img
+              src="https://skillicons.dev/icons?i=vite"
+              alt="vite"
+            />
+            <p className='text-light text-center'>Vite</p>
+          </div>
+        </div>
+        <div className="col-sm-2 mb-4 mx-3">
+          <div className="card">
+            <img
+              src="https://skillicons.dev/icons?i=vscode"
+              alt="vscode"
+            />
+            <p className='text-light text-center'>Visual Studio Code</p>
+          </div>
+        </div>
+        <div className="col-sm-2 mb-4 mx-3">
+          <div className="card">
+            <img
+              src="https://skillicons.dev/icons?i=idea"
+              alt="intellij"
+            />
+            <p className='text-light text-center'>IntelliJ IDEA</p>
           </div>
         </div>
       </div>
