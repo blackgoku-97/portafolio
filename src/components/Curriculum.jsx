d5c340f720cd5f5bfe1cec5c922482c34eefbfdd
Ricardo Perez <rpe19971210@gmail.com>
2025-03-20 16:02:49 -0300

    curriculum 4

--- src/components/Curriculum.jsx
+++ src/components/Curriculum.jsx
@@ -1,86 +1,83 @@
 export const Curriculum = () => {
-  const datosPersonales = {
-    nombre: "Ricardo Perez",
-    correoElectronico: "rpe19971210gmail.com",
-  };
-
-  const educacion = {
-    titulo: "Tecnico Analista Programador",
-    universidad: "INACAP",
-    fechaGraduacion: "2019",
-  };
-
-  const experienciaLaboral = {
-    tituloPuesto: "Puesto de ejemplo",
-    empresa: "Empresa de ejemplo",
-    fechaInicio: "2020",
-    fechaFin: "2022",
-    descripcion:
-      "Descripción breve de tus responsabilidades y logros en el puesto",
-  };
-
   return (
     <div className="container">
       <div className="row">
         <div className="col-md-12">
-          <h1 className="text-center mb-5">Curriculum</h1>
-          <div className="row">
-            <div className="col-md-4">
-              <h2 className="mb-3">Información personal</h2>
-              <ul className="list-group">
-                <li className="list-group-item">
-                  <strong>Nombre:</strong> {datosPersonales.nombre}
-                </li>
-                <li className="list-group-item">
-                  <strong>Correo electrónico:</strong>{" "}
-                  {datosPersonales.correoElectronico}
-                </li>
-              </ul>
-            </div>
-            <div className="col-md-4">
-              <h2 className="mb-3">Educación</h2>
-              <ul className="list-group">
-                <li className="list-group-item">
-                  <strong>Título de grado:</strong> {educacion.titulo}
-                </li>
-                <li className="list-group-item">
-                  <strong>Universidad:</strong> {educacion.universidad}
-                </li>
-                <li className="list-group-item">
-                  <strong>Fecha de graduación:</strong>{" "}
-                  {educacion.fechaGraduacion}
-                </li>
-              </ul>
-            </div>
-            <div className="col-md-4">
-              <h2 className="mb-3">Experiencia laboral</h2>
-              <ul className="list-group">
-                <li className="list-group-item">
-                  <strong>Título del puesto:</strong>{" "}
-                  {experienciaLaboral.tituloPuesto}
-                </li>
-                <li className="list-group-item">
-                  <strong>Empresa:</strong> {experienciaLaboral.empresa}
-                </li>
-                <li className="list-group-item">
-                  <strong>Fecha de inicio:</strong>{" "}
-                  {experienciaLaboral.fechaInicio}
-                </li>
-                <li className="list-group-item">
-                  <strong>Fecha de fin:</strong> {experienciaLaboral.fechaFin}
-                </li>
-                <li className="list-group-item">
-                  <strong>
-                    Descripción breve de tus responsabilidades y logros en el
-                    puesto:
-                  </strong>
-                  {experienciaLaboral.descripcion}
-                </li>
-              </ul>
-            </div>
-          </div>
+          <h1 className="text-center">Desarrollador Web Junior</h1>
+          <p className="text-center">
+            Desarrollador Web Junior apasionado por el desarrollo web. Con una
+            sólida formación en ingeniería informática, me caracterizo por ser
+            organizado, responsable y proactivo. Estoy ansioso por unirme a un
+            equipo dinámico y desafiante donde pueda aplicar mis habilidades y
+            continuar aprendiendo y creciendo como profesional.
+          </p>
         </div>
       </div>
+      <div className="row">
+        <div className="col-md-6">
+          <h2 className="text-center">Educación</h2>
+          <ul className="list-group">
+            <li className="list-group-item">
+              Técnico en Analista Programador, Instituto Profesional y 
+              Centro de Formacion Técnica (INACAP) (2017-2019)
+              <p>Modalidad: Presencial</p>
+            </li>
+            <li className="list-group-item">
+              Ingenieria Informática, Universidad
+              Tecnológica de Chile (INACAP) (2020-2022)
+              <p>Modalidad: Remota</p>
+            </li>
+            <li className="list-group-item">
+              Programa de AlumniOne, Alianza
+              entre Oracle y Alura (One Next Education) (2024-2025)
+              <p>Modalidad: Remota</p>
+            </li>
+          </ul>
+        </div>
+        {/* <div className="col-md-6">
+          <h2 className="text-center">Experiencia laboral</h2>
+          <ul className="list-group">
+            <li className="list-group-item">
+              <h3 className="text-center">
+                Desarrollador Web, [Nombre de la empresa] (2020-2022)
+              </h3>
+              <ul className="list-group">
+                <li className="list-group-item">
+                  Creación de aplicaciones web escalables y seguras
+                </li>
+                <li className="list-group-item">
+                  Colaboración con equipos de diseño y marketing
+                </li>
+              </ul>
+            </li>
+            <li className="list-group-item">
+              <h3 className="text-center">
+                Desarrollador Freelance (2018-2020)
+              </h3>
+              <ul className="list-group">
+                <li className="list-group-item">
+                  Creación de sitios web y aplicaciones web para clientes
+                </li>
+              </ul>
+            </li>
+          </ul>
+        </div> */}
+      </div>
+      {/* <div className="row">
+        <div className="col-md-6">
+          <h2 className="text-center">Logros</h2>
+          <ul className="list-group">
+            <li className="list-group-item">
+              Creación de una aplicación web que aumentó la conversión de leads
+              en un 25%
+            </li>
+            <li className="list-group-item">
+              Colaboración en un proyecto de código abierto que recibió más de
+              1000 estrellas en GitHub
+            </li>
+          </ul>
+        </div>
+      </div> */}
     </div>
   );
 };
