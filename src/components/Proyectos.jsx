4001f4d687663b6b434c5528092ce77c9c778864
Ricardo Perez <rpe19971210@gmail.com>
2025-03-20 00:11:46 -0300

    progreso 2

--- src/components/Proyectos.jsx
+++ src/components/Proyectos.jsx
@@ -1,63 +1,53 @@
-import axios from 'axios';
-import { useEffect, useState } from 'react';
+import './Proyectos.css';
 
 export const Proyectos = () => {
-    const [proyectos, setProyectos] = useState([]);
-    const [imagenes, setImagenes] = useState([]);
-
-    useEffect(() => {
-        axios.get(`https://api.github.com/users/blackgoku-97/repos`)
-            .then(response => {
-                setProyectos(response.data);
-                obtenerImagenes();
-            })
-            .catch(error => {
-                console.error(error);
-            });
-    }, []);
-
-    const obtenerImagenes = () => {
-        axios.get('https://picsum.photos/v2/list')
-            .then(response => {
-                setImagenes(response.data);
-            })
-            .catch(error => {
-                console.error(error);
-            });
-    };
-
-    useEffect(() => {
-        if (imagenes.length > 0 && proyectos.length > 0) {
-            const imagenesCopia = [...imagenes];
-            proyectos.forEach(proyecto => {
-                const indiceAleatorio = Math.floor(Math.random() * imagenesCopia.length);
-                proyecto.imagen = imagenesCopia.splice(indiceAleatorio, 1)[0];
-            });
-            setProyectos(proyectos);
-        }
-    }, [imagenes, proyectos]);
-
     return (
-        <div className="container mt-5">
-            <h1 className="text-center mb-5">Proyectos</h1>
-            <div className="row">
-                {proyectos.map(proyecto => (
-                    <div key={proyecto.id} className="col-md-4">
-                        <div className="card mb-3">
+        <>
+            <div className="container mt-5">
+                <div className="row justify-content-center">
+                    <div className="col-md-8">
+                        <div className="card">
                             <div className="card-header">
-                                <h2 className="card-title text-light">{proyecto.name}</h2>
+                                <h1 className="text-center text-light">Proyectos</h1>
                             </div>
                             <div className="card-body">
-                                {proyecto.imagen && (
-                                    <img src={proyecto.imagen.download_url} alt="Imagen aleatoria" className="img-fluid mb-3" />
-                                )}
-                                <p className="card-text">{proyecto.description}</p>
-                                <a href={proyecto.html_url} className="btn btn-primary">Ver en GitHub</a>
+                                <div className="row">
+                                    <div className="col-md-4">
+                                        <div className="card">
+                                        <img src="https://flcurrencyexchange.com/wp-content/uploads/2024/06/Best-time-to-exchange-currency_1.jpg" alt="Imagen del proyecto" className="card-img-top" />
+                                            <div className="card-body">
+                                                <h5 className="card-title">Conversor de Monedas</h5>
+                                                <p className="card-text">Proyecto de cambio de monedas.</p>
+                                                <a href="https://github.com/blackgoku-97/transformador_monedas" className="btn btn-primary" target='_blank'>Ver más</a>
+                                            </div>
+                                        </div>
+                                    </div>
+                                    <div className="col-md-4">
+                                        <div className="card">
+                                            <img src="https://digitek.cl/cdn/shop/articles/apple_book_iphone_digitek_blog_chile.jpg?v=1720448541&width=1600" alt="Imagen del proyecto" className="card-img-top" />
+                                            <div className="card-body">
+                                                <h5 className="card-title">Libreria</h5>
+                                                <p className="card-text">Proyecto de libros y autores.</p>
+                                                <a href="https://github.com/blackgoku-97/java-spring-libreria" className="btn btn-primary" target='_blank'>Ver más</a>
+                                            </div>
+                                        </div>
+                                    </div>
+                                    <div className="col-md-4">
+                                        <div className="card">
+                                            <img src="https://topicflower.com/blog/wp-content/uploads/2017/12/Portada-67.png" alt="Imagen del proyecto" className="card-img-top" />
+                                            <div className="card-body">
+                                                <h5 className="card-title">Foro API</h5>
+                                                <p className="card-text">Proyecto de crud de topicos.</p>
+                                                <a href="https://github.com/blackgoku-97/foroapi" className="btn btn-primary" target='_blank'>Ver más</a>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
                             </div>
                         </div>
                     </div>
-                ))}
+                </div>
             </div>
-        </div>
-    )
+        </>
+    );
 }
