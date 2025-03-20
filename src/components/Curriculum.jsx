014d06fc9210f7c08ec9fa2cddc66f10cb4caf7a
Ricardo Perez <rpe19971210@gmail.com>
2025-03-20 19:27:42 -0300

    curriculum 6

--- src/components/Curriculum.jsx
+++ src/components/Curriculum.jsx
@@ -37,6 +37,29 @@ export const Curriculum = () => {
         <div className="col-md-6">
           <h2 className="text-center">Experiencia laboral</h2>
           <ul className="list-group">
+          <li className="list-group-item">
+              <h3 className="text-center">
+                Practica Empresa Himce (2019)
+              </h3>
+              <ul className="list-group">
+                <li className="list-group-item">
+                  Desarrollo y diseño de pagina web 
+                </li>
+                <h4 className="text-center">Logros</h4>
+                <p className="text-center">Conociendo github, framework laravel y aprendiendo a usarlo</p>
+              </ul>
+              <h3 className="text-center">
+                Practica Empresa BETECH (2022)
+              </h3>
+              <ul className="list-group">
+                <li className="list-group-item">
+                  Desarrollo y diseño de pagina web con framework laravel
+                  para un buen mantenimiento 
+                </li>
+                <h4 className="text-center">Logros</h4>
+                <p className="text-center">Conociendo en primer plano la habilidad blanda: Trabajo en equipo</p>
+              </ul>
+            </li>
             <li className="list-group-item">
               <h3 className="text-center">
                 Desarrollador Web, Proyectos personales (2024-2025)
