74ee5830dd270565d376c7996889eafd1f1ca372
Ricardo Perez <rpe19971210@gmail.com>
2025-04-05 23:59:32 -0400

    frameworks estructurado

--- src/components/habilidades/components/Frameworks.jsx
+++ src/components/habilidades/components/Frameworks.jsx
@@ -1,46 +1,39 @@
+// Frameworks.js
 import { Card, Grid, Stack, Typography } from '@mui/material';
+import Tecnologia from './Tecnologia';
+
+const frameworks = [
+  { icon: 'react', nombre: 'React' },
+  { icon: 'spring', nombre: 'Spring' },
+];
 
 export const Frameworks = () => {
   return (
-    <Grid size={{ xs: 12 }}>
-      <Typography variant="h3" align="center">
-        Frameworks
-      </Typography>
-      <Grid container spacing={2} sx={{ mt: 2, gap: 2, justifyContent: 'center' }}>
-        <Grid size={{ xs: 4 }}>
-          <Card
-            sx={{
-              p: 2,
-              mb: 2,
-              borderRadius: 0,
-              boxShadow: "none",
-              border: "none",
-              backgroundColor: "transparent",
-              display: 'flex',
-              justifyContent: 'space-between',
-            }}
-          >
-            <Stack alignItems="center">
-              <img
-                src={`https://skillicons.dev/icons?i=react`}
-                alt="React"
-              />
-              <Typography variant="h6" sx={{ mt: 1, color: "white" }}>
-                React
-              </Typography>
-            </Stack>
-            <Stack alignItems="center">
-              <img
-                src={`https://skillicons.dev/icons?i=spring`}
-                alt="Spring"
-              />
-              <Typography variant="h6" sx={{ mt: 1, color: "white" }}>
-                Spring
-              </Typography>
-            </Stack>
-          </Card>
-        </Grid>
+    <Grid container spacing={2} sx={{ mt: 2, gap: 2, justifyContent: 'center' }}>
+      <Grid item xs={12}>
+        <Typography variant="h5" align="center">
+          Frameworks
+        </Typography>
+      </Grid>
+      <Grid item xs={12}>
+        <Card
+          sx={{
+            p: 2,
+            mb: 2,
+            borderRadius: 0,
+            boxShadow: "none",
+            border: "none",
+            backgroundColor: "transparent",
+            display: 'flex',
+            flexDirection: 'column',
+            alignItems: 'center',
+          }}
+        >
+          {frameworks.map((framework, index) => (
+            <Tecnologia key={index} icon={framework.icon} nombre={framework.nombre} />
+          ))}
+        </Card>
       </Grid>
     </Grid>
-  )
-}
+  );
+};
