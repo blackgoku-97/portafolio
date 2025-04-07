dadb736af7e2c537299db834d81855adbf6fd175
Ricardo Perez <rpe19971210@gmail.com>
2025-04-06 21:16:18 -0400

    estructurados 3

--- src/components/inicio/Inicio.jsx
+++ src/components/inicio/Inicio.jsx
@@ -1,5 +1,43 @@
 import { Box } from '@mui/material';
-import { Header, Imagen, Styles } from './components';
+
+const Styles = {
+  header: {
+    width: "50%",
+    height: "50vh",
+    display: "flex",
+    flexDirection: "row",
+    justifyContent: "space-between",
+    alignItems: "center",
+  },
+  title: {
+    fontSize: 36,
+    marginBottom: 20,
+  },
+  nombre: {
+    fontSize: 48,
+    marginBottom: 20,
+    fontWeight: "bold",
+    marginRight: 5,
+  },
+  imagen: {
+    width: "40%",
+    textAlign: "center",
+    marginLeft: 10,
+  },
+};
+
+const Imagen = ({ imagen }) => (
+  <Box sx={Styles.imagen}>
+    <img src={imagen} alt="img" className="img-fluid" />
+  </Box>
+);
+
+const Header = ({ title, nombre }) => (
+  <Box sx={Styles.header}>
+    <Box sx={Styles.title}>{title}</Box>
+    <Box sx={Styles.nombre}>{nombre}</Box>
+  </Box>
+);
 
 export const Inicio = () => {
 
--- src/components/inicio/components/Header.jsx
+++
@@ -1,9 +0,0 @@
-import { Box } from '@mui/material';
-import { Styles } from './Styles';
-
-export const Header = ({ title, nombre }) => (
-  <Box sx={Styles.header}>
-    <Box sx={Styles.title}>{title}</Box>
-    <Box sx={Styles.nombre}>{nombre}</Box>
-  </Box>
-);
