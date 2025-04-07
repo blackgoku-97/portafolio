d697ca26da1363eff775de9a59dfd56047136bc0
Ricardo Perez <rpe19971210@gmail.com>
2025-04-07 19:56:35 -0400

    Inicio estructurado 12

--- src/components/inicio/Inicio.jsx
+++ src/components/inicio/Inicio.jsx
@@ -85,9 +85,9 @@ const Imagen = ({ imagen }) => (
 
 const Header = ({ title, nombre }) => (
   <Box sx={{ ...Styles.texto, width: "100%", "@media (max-width: 768px)": { flexDirection: "column", alignItems: "flex-start" } }}>
-    <Box sx={{ ...Styles.tituloNombre, "@media (max-width: 768px)": { flexDirection: "column", alignItems: "flex-start" } }}>
-      <Box sx={Styles.title}>{title}</Box>
-      <Box sx={Styles.nombre}>{nombre}</Box>
+    <Box sx={{ ...Styles.tituloNombre, "@media (max-width: 768px)": { flexDirection: "column", alignItems: "flex-start", marginLeft: 0 } }}>
+      <Box sx={{ ...Styles.title, "@media (max-width: 768px)": { marginLeft: 0 } }}>{title}</Box>
+      <Box sx={{ ...Styles.nombre, "@media (max-width: 768px)": { marginLeft: 0 } }}>{nombre}</Box>
     </Box>
     <Box sx={{ ...Styles.profesion, "@media (max-width: 768px)": { marginLeft: 0 } }} style={{ marginTop: 5 }}>Desarrollador web</Box>
   </Box>
