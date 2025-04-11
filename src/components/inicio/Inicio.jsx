88d5aab193996238a8ccdd39a934948ef0e21b71
Ricardo Perez <rpe19971210@gmail.com>
2025-04-11 19:55:38 -0400

    Inicio estructurado 3

--- src/components/inicio/Inicio.jsx
+++ src/components/inicio/Inicio.jsx
@@ -2,23 +2,19 @@ import { Box, Typography } from '@mui/material';
 
 export const Inicio = () => {
   return (
-    <Box sx={{ display: 'flex', justifyContent: 'center', alignItems: 'center', height: '80vh' }}>
+    <Box sx={{ display: 'flex', justifyContent: 'center', alignItems: 'center', height: '80vh', marginTop: { xs: 10, md: 0 } }}>
       <Box sx={{
         display: 'flex',
         flexDirection: { xs: 'column', md: 'row' },
-        alignItems: 'center'
+        alignItems: { xs: 'center', md: 'center' }
       }}>
-        <Typography variant="h4" align="center" sx={{
-          marginRight: { md: 20 },
-          marginBottom: { xs: 20, md: 0 },
-          fontSize: { xs: '1.5rem', md: '2.125rem' }
-        }}>
+        <Typography variant="h4" align="center" sx={{ marginBottom: { xs: 2, md: 0 }, marginRight: { md: 20 } }}>
           ¡Bienvenido a mi sitio web! 👋
         </Typography>
         <img src="assets/img/coding.png" alt="Imagen de bienvenida" style={{
-          width: { xs: 200, md: 400 },
-          height: { xs: 200, md: 400 },
-          marginLeft: { md: 20 }
+          width: { xs: 250, md: 400 },
+          height: { xs: 250, md: 400 },
+          marginTop: { xs: 2, md: 0 }
         }} />
       </Box>
     </Box>
