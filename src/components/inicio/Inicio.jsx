051881510d84a9d05a8f5ebffb57584ce6d099be
Ricardo Perez <rpe19971210@gmail.com>
2025-04-11 23:47:40 -0400

    Inicio estructurado 9

--- src/components/inicio/Inicio.jsx
+++ src/components/inicio/Inicio.jsx
@@ -2,22 +2,21 @@ import { Grid, Typography, Container } from "@mui/material";
 
 export const Inicio = () => {
   return (
-    <Container maxWidth="lg" sx={{ height: "80vh", display: "flex", justifyContent: "center", alignItems: "center", marginTop: { xs: 10, md: 0 }, }}
+    <Container maxWidth="lg" sx={{ height: "80vh", display: "flex", justifyContent: "center", alignItems: "center", marginTop: { xs: 12, md: 0 }, }}
     >
       <Grid container spacing={{ xs: 4, md: 8 }} direction={{ xs: "column", md: "row" }} alignItems="center" justifyContent="center"
       >
         <Grid size={{ xs: 12, md: 6 }}>
           <Typography variant="h4" align="center" sx={{ marginBottom: { xs: 4, md: 0 } }}
           >
-            ¡Hola Soy Ricardo! 👋 
-            Desarrollador Web Junior
+            ¡Hola Soy Ricardo! 👋 Desarrollador Web Junior
           </Typography>
         </Grid>
         <Grid size={{ xs: 12, md: 6 }} sx={{ display: "flex", justifyContent: "center" }}
         >
           <img src="assets/img/coding.png" alt="Imagen de bienvenida" style={{
               width: "100%",
-              maxWidth: 400,
+              maxWidth: 350,
               height: "auto",
               marginLeft: { xs: 0, md: 200 },
             }}
