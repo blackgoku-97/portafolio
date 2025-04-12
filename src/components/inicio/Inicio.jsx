74b4ca659ae0a63e4843ea3675519f9dc5a36d19
Ricardo Perez <rpe19971210@gmail.com>
2025-04-11 21:49:49 -0400

    Inicio estructurado 7

--- src/components/inicio/Inicio.jsx
+++ src/components/inicio/Inicio.jsx
@@ -2,40 +2,24 @@ import { Grid, Typography, Container } from "@mui/material";
 
 export const Inicio = () => {
   return (
-    <Container
-      maxWidth="lg"
-      sx={{
-        height: "80vh",
-        display: "flex",
-        justifyContent: "center",
-        alignItems: "center",
-        marginTop: { xs: 10, md: 0 },
-      }}
+    <Container maxWidth="lg" sx={{ height: "80vh", display: "flex", justifyContent: "center", alignItems: "center", marginTop: { xs: 10, md: 0 }, }}
     >
-      <Grid
-        container
-        spacing={2}
-        direction={{ xs: "column", md: "row" }}
-        alignItems="center"
-        justifyContent="center"
+      <Grid container spacing={2} direction={{ xs: "column", md: "row" }} alignItems="center" justifyContent="center"
       >
         <Grid size={{ xs: 12, md: 6 }}>
-          <Typography
-            variant="h4"
-            align="center"
-            sx={{ marginBottom: { xs: 2, md: 0 } }}
+          <Typography variant="h4" align="center" sx={{ marginBottom: { xs: 2, md: 0 } }}
           >
             ¡Hola Soy Ricardo! 👋 <br /> Desarrollador Web Junior
           </Typography>
         </Grid>
-        <Grid
-          size={{ xs: 12, md: 6 }}
-          sx={{ display: "flex", justifyContent: "center" }}
+        <Grid size={{ xs: 12, md: 6 }} sx={{ display: "flex", justifyContent: "center" }}
         >
-          <img
-            src="assets/img/coding.png"
-            alt="Imagen de bienvenida"
-            style={{ width: "100%", maxWidth: 400, height: "auto", marginLeft: 200 }}
+          <img src="assets/img/coding.png" alt="Imagen de bienvenida" style={{
+              width: "100%",
+              maxWidth: 400,
+              height: "auto",
+              marginLeft: { xs: 0, md: 200 },
+            }}
           />
         </Grid>
       </Grid>
