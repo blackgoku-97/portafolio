c1a53213b8a1c640c0bbf97100f63fb5f87fa048
Ricardo Perez <rpe19971210@gmail.com>
2025-10-30 21:57:00 -0300

    mejorando 14

--- src/pages/Curriculum.tsx
+++ src/pages/Curriculum.tsx
@@ -1,5 +1,5 @@
 import jsPDF from "jspdf"
-import html2canvas from "html2canvas"
+import html2canvas from "html2canvas-pro"
 import CurriculumContent from "./CurriculumContent"
 import { FaDownload } from "react-icons/fa"
 
